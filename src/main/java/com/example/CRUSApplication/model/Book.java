51932d0c2772acb27cb1802cd9a1a31c63663a8f
Chiranthan <Chiranthangowda4224@gmail.com>
2023-01-21 16:19:14 +0530

    First Commit

---
+++ src/main/java/com/example/CRUSApplication/model/Book.java
@@ -0,0 +1,25 @@
+package com.example.CRUSApplication.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="Books")
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@ToString
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private  Long id;
+
+    private String title;
+
+    private String author;
+
+
+}
